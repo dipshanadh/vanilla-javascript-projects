25f508e78c73547423f16ae50250d3d56774e061
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2022-09-25 10:53:01 +0545

    updated background-color-switcher JavaScript

--- background-color-switcher/script.js
+++ background-color-switcher/script.js
@@ -1,42 +1,11 @@
-let body = document.querySelector("body");
-let button = document.querySelectorAll("button");
-let elements = document.querySelectorAll("*");
-// console.log(elements);
+const body = document.querySelector("body"),
+	buttons = document.querySelectorAll("button")
 
-body.addEventListener("click", changeBackground);
+buttons.forEach(
+	button => (button.onclick = () => changeColor(button.className))
+)
 
-function changeBackground(e) {
-	// console.log(e.target);
-
-	let className = e.target.className;
-	// console.log(className);
-
-	switch (className) {
-		case "black":
-			body.style.background = "var(--black)";
-			changeColor();
-			break;
-		case "green":
-			body.style.background = "var(--green)";
-			changeColor();
-			break;
-		case "blue":
-			body.style.background = "var(--blue)";
-			changeColor();
-			break;
-		case "pink":
-			body.style.background = "var(--pink)";
-			changeColor();
-			break;
-		case "brown":
-            body.style.background = "var(--brown)"
-			changeColor();
-			break;
-	}
-}
-
-function changeColor() {
-	elements.forEach((element) => {
-		element.style.color = "var(--white)";
-	});
+function changeColor(className) {
+	body.style.background = `var(--${className})`
+	body.style.color = "white"
 }
