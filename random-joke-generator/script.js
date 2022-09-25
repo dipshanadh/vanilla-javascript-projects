e603b0b15975f51218713bb30d8d5b9ac35b1d12
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2022-09-25 11:40:11 +0545

    refactor

--- random-joke-generator/script.js
+++ random-joke-generator/script.js
@@ -1,28 +1,29 @@
-let jokeDiv = document.querySelector("#jokeDiv");
-const button = document.querySelector("#getJoke");
+const jokeDiv = document.querySelector("#jokeDiv"),
+	button = document.querySelector("#getJoke")
 
-window.addEventListener("DOMContentLoaded", () => getJoke());
-button.addEventListener("click", () => getJoke());
+button.addEventListener("click", getJoke)
 
 function getJoke() {
-	let xhr = new XMLHttpRequest();
-	let url = "https://api.chucknorris.io/jokes/random";
+	const xhr = new XMLHttpRequest(),
+		url = "https://api.chucknorris.io/jokes/random"
 
-	xhr.open("GET", url, true);
-	xhr.send();
+	xhr.open("GET", url, true)
+	xhr.send()
 
 	xhr.onreadystatechange = function () {
 		if (this.readyState < 4) {
-			jokeDiv.innerHTML = "Loading...";
+			jokeDiv.innerHTML = "Loading..."
 		} else if (this.status == 200 && this.readyState == 4) {
-			let data = JSON.parse(this.responseText);
-			jokeDiv.textContent = `${data.value}`;
+			const data = JSON.parse(this.responseText)
+			jokeDiv.textContent = `${data.value}`
 		} else if (this.status !== 200) {
-			jokeDiv.textContent = `Error ${this.status}: ${this.statusText}`;
+			jokeDiv.textContent = `Error ${this.status}: ${this.statusText}`
 		}
-	};
+	}
 
-	xhr.onerror = function () {
-		jokeDiv.textContent = "There was error fetching the data.";
-	};
+	xhr.onerror = () => {
+		jokeDiv.textContent = "There was error fetching the data."
+	}
 }
+
+getJoke()
