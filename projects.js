55e4f0ed0162e0460eed9da32063094b5b7f899c
Dipshan Adhikari <dipshanadh@gmail.com>
2024-03-10 21:27:40 +0545

    Complete

---
+++ projects.js
@@ -0,0 +1,74 @@
+const projects = [
+	{
+		name: "Navigation Footer And LightDark Mode",
+		link: "Navigation-Footer-and-LightDark-mode",
+	},
+	{
+		name: "Advice Generator",
+		link: "advice-generator",
+	},
+	{
+		name: "Background Color Switcher",
+		link: "background-color-switcher",
+	},
+	{
+		name: "Color Flipper",
+		link: "color-flipper",
+	},
+	{
+		name: "Contact Us",
+		link: "contact-us",
+	},
+	{
+		name: "Countdown Timer",
+		link: "countdown-timer",
+	},
+	{
+		name: "Digital Clock",
+		link: "digital-clock",
+	},
+	{
+		name: "Faq Accordion",
+		link: "faq-accordion",
+	},
+	{
+		name: "Filter Menu",
+		link: "filter-menu",
+	},
+	{
+		name: "Github User Finder",
+		link: "github-user-finder",
+	},
+	{
+		name: "Input Separator",
+		link: "input-separator",
+	},
+	{
+		name: "Newletter",
+		link: "newletter",
+	},
+	{
+		name: "Pomodoro Timer",
+		link: "pomodoro-timer",
+	},
+	{
+		name: "Random Joke Generator",
+		link: "random-joke-generator",
+	},
+	{
+		name: "Stopwatch",
+		link: "stopwatch",
+	},
+	{
+		name: "Todo Lister",
+		link: "todo-lister",
+	},
+	{
+		name: "Url Shortener",
+		link: "url-shortener",
+	},
+	{
+		name: "Years Facts App",
+		link: "years-facts-app",
+	},
+];
