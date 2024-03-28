f109f9d7ad5f2412b25df3be87eff61db377841c
Dipshan Adhikari <dipshanadh@gmail.com>
2024-03-28 13:55:13 +0545

    Fix links

--- projects.js
+++ projects.js
@@ -72,3 +72,29 @@ const projects = [
 		link: "years-facts-app",
 	},
 ];
+
+const container = document.getElementById("projects");
+
+container.innerHTML = projects
+	.map(
+		project =>
+			`
+<div
+	class="h-48 rounded-2xl flex justify-center items-center relative bg-[url('./screenshots/${project.link}.png')] bg-cover bg-center"
+>
+	<div
+		class="w-full h-full absolute top-0 left-0 bg-black opacity-50"
+	></div>
+
+	<p class="drop-shadow-md">${project.name}</p>
+
+	<a
+		href="./${project.link}/index.html"
+		class="absolute right-4 top-4 px-2 py-1 bg-orange roundetext-white text-sm font-semibold hover:underline underline-offset-2"
+	>
+		See live
+	</a>
+</div>
+`
+	)
+	.join("");
